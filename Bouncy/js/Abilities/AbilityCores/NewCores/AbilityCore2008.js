fa2cbf61f9ddbe85bde18b6f5772e5d2c3332ef6
TheOneTrueGod <jprevoe@gmail.com>
2018-08-02 21:25:17 -0700

    Adjusted style for drill

---
+++ Bouncy/js/Abilities/AbilityCores/NewCores/AbilityCore2008.js
@@ -0,0 +1,60 @@
+// TODO:
+// [] Change the weapon type
+// [] Change the icon
+// [] Change the cooldown
+// [] Change the style
+// [] Change the rawAbil
+class AbilityCore2008 extends AbilityCore {
+  static BuildAbilityChild(level) {
+    let num_hits = 4;
+    let hitDamage = Math.floor(NumbersBalancer.getAbilityDamage(level, .8)) / num_hits;
+    const rawAbil = {
+      name: 'Drill Shot',
+      description: 'Shoots a projectile that passes through enemies.<br>' +
+        'It deals [[hit_effects[0].base_damage]] damage to up to [[num_hits]] targets.',
+      card_text_description: '[[num_hits]] X [[hit_effects[0].base_damage]]',
+      ability_type: AbilityDef.AbilityTypes.PROJECTILE,
+      shape: ProjectileAbilityDef.Shapes.SINGLE_SHOT,
+      projectile_type: ProjectileShape.ProjectileTypes.STANDARD,
+      collision_behaviours: [
+        {behaviour: CollisionBehaviour.PASSTHROUGH, count: num_hits - 1},
+      ],
+      num_hits,
+      destroy_on_wall: [],
+      wall_bounces: 1,
+      hit_effects: [
+        {
+          base_damage: hitDamage,
+          effect: ProjectileShape.HitEffects.DAMAGE,
+        }
+      ],
+      icon: "/Bouncy/assets/icons/icon_plain_drill.png",
+    };
+
+    let cooldown = this.getCooldown();
+    if (cooldown !== null) {
+      rawAbil.charge = cooldown;
+    }
+
+    rawAbil.style = this.createAbilityStyle().build();
+    return AbilityDef.createFromJSON(rawAbil);
+  }
+
+  static createAbilityStyle() {
+    return (new AbilitySheetSpriteAbilityStyleBuilder)
+      .setSheet('bullet_sheet')
+      // 29 -> 37
+      .setCoordNums(64, 301, 64+8, 320)
+      .setRotation(Math.PI / 2)
+  }
+
+  static getCooldown() {
+    return {initial_charge: -1, max_charge: 2, charge_type: "TURNS"};
+  }
+
+  static GetCardDeckType() {
+    return CardDeckTypes.DEFENDER;
+  }
+}
+
+AbilityCore.coreList[2008] = AbilityCore2008;
